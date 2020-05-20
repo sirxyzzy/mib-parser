98437c4be4315547f6dfb88081aa75064a58aa1b
Andy Philpotts <andy@failfree.net>
2020-05-20 17:45:28 -0400

    test helper cleanup

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 use pest::Parser;
-use pest::iterators::Pairs;
+use pest::iterators::{Pairs,Pair};
 use pest::RuleType;
 use regex::Regex;
 use indextree::Arena;
@@ -19,12 +19,13 @@ pub struct ObjectIdentifierNode {
 pub fn parse_mib(mib_text: &str) {
     println!("Parsing mib of size {}", mib_text.len());
     let _arena = &mut Arena::<ObjectIdentifierNode>::new();
-    let _ = MibParser::parse(Rule::main, mib_text);
+    let pairs = MibParser::parse(Rule::main, mib_text).unwrap();
+    print_pairs(pairs, 0)
 }
 
 #[allow(dead_code)]
-fn get_quoted_string<R: RuleType>(mut pairs: Pairs<R>) -> String {
-    let raw = pairs.next().unwrap().into_inner().as_str().to_owned();
+fn get_quoted_string<R: RuleType>(pair: Pair<R>) -> String {
+    let raw = pair.into_inner().as_str().to_owned();
 
     // Replace double quotes with single
     let raw = raw.replace("\"\"", "\"");
@@ -36,118 +37,281 @@ fn get_quoted_string<R: RuleType>(mut pairs: Pairs<R>) -> String {
 }
 
 #[allow(dead_code)]
-fn get_number<R: RuleType>(mut pairs: Pairs<R>) -> u64 {
-    pairs.next().unwrap().as_str().parse::<u64>().unwrap()
+fn get_number<R: RuleType>(pair: Pair<R>) -> u64 {
+    pair.as_str().parse::<u64>().unwrap()
 }
 
 #[allow(dead_code)]
-fn get_hex_number<R: RuleType>(mut pairs: Pairs<R>) -> u64 {
-    let s = pairs.next().unwrap().as_str();
+fn get_hex_number<R: RuleType>(pair: Pair<R>) -> u64 {
+    let s = pair.as_str();
     let len = s.len();
     u64::from_str_radix(&s[..len-2], 16).unwrap()
 }
 
 #[allow(dead_code)]
-fn get_bin_number<R: RuleType>(mut pairs: Pairs<R>) -> u64 {
-    let s = pairs.next().unwrap().as_str();
+fn get_bin_number<R: RuleType>(pair: Pair<R>) -> u64 {
+    let s = pair.as_str();
     let len = s.len();
     u64::from_str_radix(&s[..len-2], 2).unwrap()
 }
 
 #[allow(dead_code)]
-fn get_identifier<R: RuleType>(mut pairs: Pairs<R>) -> String {
-    pairs.next().unwrap().as_str().to_owned()
+fn get_identifier<R: RuleType>(pair: Pair<R>) -> String {
+    pair.as_str().to_owned()
 }
 
 #[allow(dead_code)]
-fn print_pairs<R: RuleType>(pairs: Pairs<R>) {
-    print_pairs_helper(pairs, 0)
+fn print_pair<R: RuleType>(pair: Pair<R>) {
+    println!("{:?} '{}'", pair.as_rule(), pair.as_str());
+    print_pairs(pair.into_inner(), 1)
 }
 
-fn print_pairs_helper<R: RuleType>(pairs: Pairs<R>, level: usize) {
+fn print_pairs<R: RuleType>(pairs: Pairs<R>, level: usize) {
     let indent = " ".repeat(level*4);
     for pair in pairs {
         // A pair is a combination of the rule which matched and a span of input
         println!("{}{:?} '{}'", indent, pair.as_rule(), pair.as_str());
 
         // A pair can be converted to an iterator of the tokens which make it up:
-        print_pairs_helper(pair.into_inner(), level+1);
+        print_pairs(pair.into_inner(), level+1);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::Rule;
+    use pest::Parser;
 
     #[test]
     fn number() {
-        let number = get_number(MibParser::parse(Rule::number_string, "12345678XYZ").unwrap());
+        let number = get_number(parse(Rule::number_string, "12345678"));
         assert_eq!(number, 12345678);
     }
 
     #[test]
     fn not_a_number() {
-        assert!(MibParser::parse(Rule::number_string, "-1234XYZ").is_err(), "Expected an error!");
+        parse_fail(Rule::number_string, "A1234");
     }
 
     #[test]
     fn quoted_string_1() {
-        let result = get_quoted_string(MibParser::parse(Rule::quoted_string, r#""this is a quoted string""#).unwrap());
+        let result = get_quoted_string(parse(Rule::quoted_string, r#""this is a quoted string""#));
         assert_eq!(result, "this is a quoted string");
     }
 
     #[test]
     fn quoted_string_2() {
-        let result = get_quoted_string(MibParser::parse(Rule::quoted_string, r#""this is a ""quoted"" string""#).unwrap());
+        let result = get_quoted_string(parse(Rule::quoted_string, r#""this is a ""quoted"" string""#));
         assert_eq!(result, r#"this is a "quoted" string"#);
     }
 
     #[test]
     fn quoted_string_3() {
-        let result = get_quoted_string(MibParser::parse(Rule::quoted_string, "\"this is a    \n   quoted string\"").unwrap());
+        let result = get_quoted_string(parse(Rule::quoted_string, "\"this is a    \n   quoted string\""));
         assert_eq!(result, "this is a\nquoted string");
 
-        let result = get_quoted_string(MibParser::parse(Rule::quoted_string, "\"this is a    \r\n   quoted string\"").unwrap());
+        let result = get_quoted_string(parse(Rule::quoted_string, "\"this is a    \r\n   quoted string\""));
         assert_eq!(result, "this is a\nquoted string");
     }
 
     #[test]
     fn binary_string() {
-        let result = get_bin_number(MibParser::parse(Rule::binary_string, "11110000'b").unwrap());
+        let result = get_bin_number(parse(Rule::binary_string, "11110000'b"));
         assert_eq!(result, 0b11110000);
     }
 
     #[test]
     fn hex_string() {
-        let result = get_hex_number(MibParser::parse(Rule::hex_string, "DEADBEEF'H").unwrap());
+        let result = get_hex_number(parse(Rule::hex_string, "DEADBEEF'H"));
         assert_eq!(result, 0xDEADBEEF);
     }
 
     #[test]
     fn identifier_0() {
-        let identifier = get_identifier(MibParser::parse(Rule::identifier, "ab1ur_d-gh0").unwrap());
+        let identifier = get_identifier(parse(Rule::identifier, "ab1ur_d-gh0"));
         assert_eq!(identifier, "ab1ur_d-gh0");
     }
 
     #[test]
     fn identifier_1() {
-        assert!(MibParser::parse(Rule::identifier, "0abc").is_err(), "Expected an error for identifier 0abc!");
+        parse_fail(Rule::identifier, "0abc");
     }
 
     #[test]
     fn identifier_2() {
-        assert!(MibParser::parse(Rule::identifier, "_abc").is_err(), "Expected an error for identifier _abc!");
+        parse_fail(Rule::identifier, "_abc");
     }
 
     #[test]
     fn object_id_0() {
-        let pairs = MibParser::parse(Rule::value_assignment, "synology OBJECT IDENTIFIER ::= { enterprises 6574 }").unwrap();
-
-        print_pairs(pairs)
+        let pair = parse(Rule::value_assignment, "synology OBJECT IDENTIFIER ::= { enterprises 6574 }");
+        print_pair(pair)
     }
 
     #[test]
     fn sequence_1() {
-        let _pair = MibParser::parse(Rule::sequence_of_type, "SEQUENCE OF wibble").unwrap().next().unwrap();
+        let pair = parse(Rule::sequence_of_type, "SEQUENCE OF wibble");
+        print_pair(pair)
+    }
+
+    #[test]
+    fn snmp_update() {
+        let input = r#"LAST-UPDATED "201309110000Z""#;
+        let pair = parse(Rule::snmp_update_part, input);
+        print_pair(pair)
+    }
+    #[test]
+    fn x_identifier()
+    {
+        let input = "synoDisk";
+        let pair = parse(Rule::identifier, input);
+        print_pair(pair)
+    }
+
+    #[test]
+    fn x_some_type() {
+        let input = r#"MODULE-IDENTITY
+        LAST-UPDATED "201309110000Z"
+        ORGANIZATION "www.synology.com"
+        CONTACT-INFO
+             "postal:   Jay Pan
+              email:    jaypan@synology.com"
+        DESCRIPTION
+            "Characteristics of the disk information"
+        REVISION     "201309110000Z"
+        DESCRIPTION
+            "Second draft."
+        "#;
+
+        let pair = parse(Rule::some_type, input);
+        print_pair(pair)
+    }
+
+    #[test]
+    fn value() {
+        let input = "{ synology 2 }";
+
+        let pair = parse(Rule::value, input);
+        print_pair(pair)
+    }
+
+    #[test]
+    fn x_identified_type_1() {
+        let input = r#"
+        synoDisk MODULE-IDENTITY
+            LAST-UPDATED "201309110000Z"
+            ORGANIZATION "www.synology.com"
+            CONTACT-INFO
+                 "postal:   Jay Pan
+                  email:    jaypan@synology.com"
+            DESCRIPTION
+                "Characteristics of the disk information"
+            REVISION     "201309110000Z"
+            DESCRIPTION
+                "Second draft."
+        "#;
+
+        let pair = parse(Rule::x_identified_type, input);
+        print_pair(pair)        
+    }
+
+    #[test]
+    fn x_identified_type_2() {
+        let input = "synoDisk BOOLEAN";
+        let pair = parse(Rule::x_identified_type, input);
+        print_pair(pair)        
+    }
+
+    #[test]
+    fn value_assignment() {
+        let input = r#"
+        synoDisk MODULE-IDENTITY
+            LAST-UPDATED "201309110000Z"
+            ORGANIZATION "www.synology.com"
+            CONTACT-INFO
+                 "postal:   Jay Pan
+                  email:    jaypan@synology.com"
+            DESCRIPTION
+                "Characteristics of the disk information"
+            REVISION     "201309110000Z"
+            DESCRIPTION
+                "Second draft."
+            ::= { synology 2 }
+        "#;
+
+        let pair = parse(Rule::value_assignment, input);
+        print_pair(pair)        
+    }
+
+    #[test]
+    fn import_list() {
+        let input = r#"IMPORTS
+        OBJECT-GROUP, MODULE-COMPLIANCE
+                    FROM SNMPv2-CONF
+        enterprises, MODULE-IDENTITY, OBJECT-TYPE, Integer32
+                    FROM SNMPv2-SMI;
+        "#;
+
+        let pair = parse(Rule::import_list, input);
+        print_pair(pair)
+    }
+
+    #[test]
+    fn xassignment() {
+        let input = r#"
+        synoDisk MODULE-IDENTITY
+            LAST-UPDATED "201309110000Z"
+            ORGANIZATION "www.synology.com"
+            CONTACT-INFO
+                 "postal:   Jay Pan
+                  email:    jaypan@synology.com"
+            DESCRIPTION
+                "Characteristics of the disk information"
+            REVISION     "201309110000Z"
+            DESCRIPTION
+                "Second draft."
+            ::= { synology 2 }
+        "#;
+
+        let pair = parse(Rule::value_assignment, input);
+        print_pair(pair)        
+    }
+
+    #[test]
+    fn module_body() {
+        let input = r#"IMPORTS
+            OBJECT-GROUP, MODULE-COMPLIANCE
+                        FROM SNMPv2-CONF
+            enterprises, MODULE-IDENTITY, OBJECT-TYPE, Integer32
+                        FROM SNMPv2-SMI;
+        
+        synoDisk MODULE-IDENTITY
+            LAST-UPDATED "201309110000Z"
+            ORGANIZATION "www.synology.com"
+            CONTACT-INFO
+                 "postal:   Jay Pan
+                  email:    jaypan@synology.com"
+            DESCRIPTION
+                "Characteristics of the disk information"
+            REVISION     "201309110000Z"
+            DESCRIPTION
+                "Second draft."
+            ::= { synology 2 }
+        "#;
+
+        let pair = parse(Rule::module_body, input);
+        print_pair(pair)
+    }
+
+    fn parse(rule: Rule, input: &str) -> Pair<Rule> {
+        let pair = MibParser::parse(rule, input).unwrap().next().unwrap();
+        assert_eq!(pair.as_rule(), rule);
+        assert_eq!(pair.as_str(), input, "Expected rule({:?}) to fully consume '{}'", rule, input);
+        pair
+    }
+
+    fn parse_fail(rule: Rule, input: &str) {
+        assert!(MibParser::parse(rule, input).is_err(), "Expected rule({:?}) to fail to parse '{}'", rule, input);
     }
 }
